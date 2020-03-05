e0791344e577c986072e629d9994662747a2bcc5
YahyaStihi18 <61831390+YahyaStihi18@users.noreply.github.com>
2020-03-05 13:20:34 +0100

    Add files via upload
    a script that create different shapes with the turtle module

---
+++ shapes drawing.py
@@ -0,0 +1,12 @@
+#play with number's to change the shape
+import turtle
+
+turtle.Turtle()
+turtle.speed(999)
+x = turtle
+add = 0
+while True:
+    x.forward(add)
+    x.right(100)
+    add = add + 0.1
+
